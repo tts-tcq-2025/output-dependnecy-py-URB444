90661d0229296e98439cfa3f7ffe0cc098c2b182
URB444 <87970004+URB444@users.noreply.github.com>
2025-07-29 09:26:55 +0530

    Update tshirts.py

--- tshirts.py
+++ tshirts.py
@@ -11,4 +11,8 @@ def size(cms):
 assert(size(37) == 'S')
 assert(size(40) == 'M')
 assert(size(43) == 'L')
+assert(size(38) == 'M')
+assert(size(42) == 'L')
+assert(size(39) == 'S')
+assert(size(100) == 'S')
 print("All is well (maybe!)")
